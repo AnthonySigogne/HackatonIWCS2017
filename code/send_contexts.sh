d0683a1c8246b75d110984207ec1f1cee67accef
anthony sigogne <anthonysigogne@MBP-de-anthony.lan>
2017-09-20 15:27:43 +1100

    script d'envoi des contextes

--- code/send_contexts.sh
+++ code/send_contexts.sh
@@ -1,15 +1,3 @@
-urlencodepipe() {
-  local LANG=C; local c; while IFS= read -r c; do
-    case $c in [a-zA-Z0-9.~_-]) printf "$c"; continue ;; esac
-    printf "$c" | od -An -tx1 | tr ' ' % | tr -d '\n'
-  done <<EOF
-$(fold -w1)
-EOF
-  echo
-}
-
-urlencode() { printf "$*" | urlencodepipe ;}
-
 while read p; do
   echo $p
   x=`python2 -c "import urllib;print urllib.quote(raw_input())" <<< "$p"`
